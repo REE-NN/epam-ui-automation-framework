8798d8f2087bc933884645be58f6e5b0a3bac76f
REE-NN <ree.post@yandex.ru>
2025-09-17 19:32:51 +0300

    feat(driver): add DriverFactory with browser selection

---
+++ src/main/java/driver/DriverFactory.java
@@ -0,0 +1,29 @@
+package driver;
+
+import config.ConfProperties;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+
+    public static WebDriver createDriver() {
+        String browser = ConfProperties.getProperty("browser").toLowerCase();
+
+        switch (browser) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                return new ChromeDriver();
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                return new EdgeDriver();
+            default:
+                throw new IllegalArgumentException("Unknown browser: " + browser);
+        }
+    }
+}
